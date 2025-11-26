e101f881fcd849bc87ec2e87bcb090b2c9d9724c
nastushal1102-oss <nastushal1102@gmail.com>
2025-11-26 13:39:36 +0200

    Add files via upload

---
+++ LZ2_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>       // Для sin, cos, log (натуральний логарифм)
+#include <windows.h>   // Для кирилиці
+
+using namespace std;
+
+int main() {
+    // Налаштування української мови
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double x, y;
+
+    cout << "Введіть значення x: ";
+    cin >> x;
+
+    // Перевірка умови
+    if (x <= 0) {
+        // Якщо x менше або дорівнює 0: y = sin(cos(x))
+        y = sin(cos(x));
+        cout << "Використано формулу: y = sin(cos(x))" << endl;
+    }
+    else {
+        // Якщо x більше 0: y = cos(ln(x))
+        // У C++ натуральний логарифм ln(x) — це функція log(x)
+        y = cos(log(x));
+        cout << "Використано формулу: y = cos(ln(x))" << endl;
+    }
+
+    cout << "Результат y = " << y << endl;
+
+    return 0;
+}
